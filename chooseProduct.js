192b624431e1b8eaef73be5e02bf8345bc630b9d
Romeu maier <romeumaier@gmail.com>
2023-11-21 11:35:04 -0300

    implementa

--- chooseProduct.js
+++ chooseProduct.js
@@ -1,5 +1,13 @@
 function chooseProduct(segment) {
-    // Add your solution here!
+   switch(segment){
+    case "Beverage" : 
+    return "Soda";
+    case "Fruit": return "Apple";
+    case "Hygiene": return "Soap";
+
+    default: return "Product not found";
+
+       }
 }
 
 module.exports = chooseProduct;
